b0008c49cd1a099b7166f6491a867759d5679b5c
luweijia <luweijia1013@sjtu.edu.cn>
2019-08-09 00:29:59 +0100

    write after 30 tweets

--- twitterapidata/streaming.py
+++ twitterapidata/streaming.py
@@ -9,48 +9,51 @@ import decimal
 
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
-consumer_key = 'ZqNtCbyxoWMAkqvEUVeuVbJzM'
-consumer_secret = '25NKzjVS9AT5KKqMBG8XZqiBYbUvb5aVOkuTihFJphwpscDdGZ'
-# consumer_key="tTycUYRidT6tx0LqRg7KkvVf3"
-# consumer_secret="dA7mlmK8pC3Rjys3angNL71zRpQLmfTb6pxqmCybuhoO5ZLdJK"
+# consumer_key = 'ZqNtCbyxoWMAkqvEUVeuVbJzM'
+# consumer_secret = '25NKzjVS9AT5KKqMBG8XZqiBYbUvb5aVOkuTihFJphwpscDdGZ'
+consumer_key="tTycUYRidT6tx0LqRg7KkvVf3"
+consumer_secret="dA7mlmK8pC3Rjys3angNL71zRpQLmfTb6pxqmCybuhoO5ZLdJK"
 
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
-access_token = '1091008892866318337-RvNvRsrMR4sADqM3b2Rbe4Z63Q8ruh'
-access_token_secret = 'ZndXLIfY1t4Jq7HH9fTVWIqZCmjl0dazykRuEtQhh7JCI'
-# access_token="982767300-LraJlggxgvA60h2uVY8TP2vekc3Geu4Ck1NJLNJ6"
-# access_token_secret="4Xbkp8pwuDQ9LArejXfdRnsA70lB9CDOg0MF1It54FNGa"
+# access_token = '1091008892866318337-RvNvRsrMR4sADqM3b2Rbe4Z63Q8ruh'
+# access_token_secret = 'ZndXLIfY1t4Jq7HH9fTVWIqZCmjl0dazykRuEtQhh7JCI'
+access_token="982767300-LraJlggxgvA60h2uVY8TP2vekc3Geu4Ck1NJLNJ6"
+access_token_secret="4Xbkp8pwuDQ9LArejXfdRnsA70lB9CDOg0MF1It54FNGa"
 result = []
 
-tweets = []
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
     This is a basic listener that just prints received tweets to stdout.
     """
+
+    tweets = []
+
     def on_data(self, data):
         tweet = json.loads(data, parse_float = decimal.Decimal)
+        print(tweet['text'])
         if tweet['lang'] == 'en':
-            tweet.encode('utf-8')
-            # tweets.append(' '.join(tweet['text'].split()))
-            print(' '.join(tweet['text'].split()))
-            with open('data/apple', 'a') as f:
-                f.write(' '.join(tweet['text'].split())+'\n')
+            tweet['text'].encode('utf-8')
+            StdOutListener.tweets.append(' '.join(tweet['text'].split()))
+            # print(' '.join(tweet['text'].split()))
+            # with open('data/apple', 'a') as f:
+            #     f.write(' '.join(tweet['text'].split())+'\n')
 
-        # print(len(tweets))
-        # STEP = 3
-        # if len(tweets) > 0 and len(tweets) % STEP == 0:
-        #     with open('data/apple','a') as f:
-        #         for i in range(STEP):
-        #             f.write(tweets.pop(0))
-            return True
+            # print(len(StdOutListener.tweets))
+            STEP = 30
+            if len(StdOutListener.tweets) > 0 and len(StdOutListener.tweets) % STEP == 0:
+                with open('data/apple','a') as f:
+                    for i in range(STEP):
+                        f.write(StdOutListener.tweets.pop(0))
+                        f.write('\n')
+        return True
 
         # tweet变量就是获取到的数据
 
-        return True
 
     def on_error(self, status):
-        print(status)
+        print('error',status)
 
 if __name__ == '__main__':
     l = StdOutListener()
